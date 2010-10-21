9a255f072eb953447b1f61748d8f94f92dd0d77e
Nikola Chochkov <nikola@howkul.info>
2010-10-21 08:48:53 +0200

    helper name changed

--- helpers.rb
+++ helpers.rb
@@ -2,7 +2,7 @@ helpers do
   def config option
     'uploads/' if option == 'dir' 
   end
-  def hash
+  def random_string
     (0...10).map{ ('a'..'z').to_a[rand(26)] }.join.concat(Time.now.usec.to_s)
   end
 end
